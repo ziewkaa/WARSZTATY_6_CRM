762ddddc890719c0b86ef5007363fb121af7e609
ziewkaa <eva.zientara@gmail.com>
2018-05-18 14:32:31 +0200

    Adds basic app files

---
+++ src/main/java/pl/coderslab/cmr/CmrApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.cmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmrApplication.class, args);
+    }
+}
